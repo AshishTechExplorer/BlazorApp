47dce0ab4df90f7c0a4cf10642ec84967c018f10
Ashish <ashishpant2024@gmail.com>
2024-09-08 19:15:41 +0530

    blazor application

---
+++ Models/UserDetails.cs
@@ -0,0 +1,50 @@
+﻿namespace AspNetCoreBlazorTest.Models
+{
+    public class UserDetails
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string? UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public Address Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Website { get; set; }
+        public Company Company { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+
+        public string Suite { get; set; }
+
+        public string? City { get; set; }
+
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+
+        public Geo Geo { get; set; }
+    }
+
+
+
+    public class Geo
+    {
+        public decimal Lat { get; set; }
+        public decimal Lng { get; set; }
+    }
+
+    public class Company
+    {
+        public string Name { get; set; }
+        public string CatchPhrase { get; set; }
+        public string Bs { get; set; }
+    }
+}
